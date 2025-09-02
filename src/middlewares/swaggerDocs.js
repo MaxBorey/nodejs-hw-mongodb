17bb8ebe0fbb2de54f1a54c81ddaf0f8181a9fb9
maxborey <maxborey@gmail.com>
2025-09-02 20:59:35 +0300

    Documention about contacts

---
+++ src/middlewares/swaggerDocs.js
@@ -0,0 +1,10 @@
+// import createHttpError from 'http-errors';
+import swaggerUI from 'swagger-ui-express';
+import fs from 'node:fs';
+
+import { SWAGGER_PATH } from '../constants/index.js';
+
+export const swaggerDocs = () => {
+    const swaggerDoc = JSON.parse(fs.readFileSync(SWAGGER_PATH).toString());
+    return [...swaggerUI.serve, swaggerUI.setup(swaggerDoc)];
+};
